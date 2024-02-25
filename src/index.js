558f7faea254a4454e1fa9d62f7f564ab707cfd8
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-02-24 22:04:07 -0500

    Loading css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import "./style.css";
 // import your function
 import myName from "./myName";
 
@@ -7,6 +8,7 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   return element;
 }
