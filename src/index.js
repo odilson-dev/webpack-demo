76b2a8130e976a4255111c0e5ac8bcd9a97ed3d4
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-02-26 12:07:53 -0500

    Customize parser of JSON modules

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import Notes from "./data.csv";
 import Data from "./data.xml";
 import Icon from "./icon.png";
 import "./style.css";
+import toml from "./data.toml";
+import yaml from "./data.yaml";
+import json from "./data.json5";
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 // import your function
 import myName from "./myName";
 
