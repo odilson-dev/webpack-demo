277f476ed2dc312e00820cfa408ea81f98776708
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-02-26 11:52:18 -0500

    Loading data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import _ from "lodash";
+import Notes from "./data.csv";
+import Data from "./data.xml";
 import Icon from "./icon.png";
 import "./style.css";
-
 // import your function
 import myName from "./myName";
 
@@ -17,6 +18,8 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
